b87123c07ff73cd2f9fe45196525612b0e66ad28
Jasper De Laet <jdlaet@gmail.com>
2020-09-23 20:35:20 +0200

    not_fibonacci

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -3,21 +3,28 @@
 using namespace std;
 
 void printFibonacci(int n){
-    int f0 = 0;
+    int f0 = 1;
     int f1 = 1;
-    int f2;
+    int f2 = 1;
+    int f3;
     if(n == 0){
         cout << f0 << endl;
-    } else{
+    } else if(n == 1) {
+    	cout << f0 << endl;
+        cout << f1 << endl;
+    }
+    else{
         cout << f0 << endl;
         cout << f1 << endl;
+        cout << f2 << endl;
         for(int i = 0 ; i < n - 1; i++){
-            f2 = f1 + f0;
+            f3 = f2 + f1 + f0;
             
-            cout << f2 << endl;
+            cout << f3 << endl;
             
             f0 = f1;
             f1 = f2;
+            f2 = f1;
         }
     }
 }
